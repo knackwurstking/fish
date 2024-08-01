19f1e0fb91b625537281907f80da7a4cdfaa4707
Udo Bauer <knackwurstking.tux@gmail.com>
2024-08-01 13:51:34 +0200

    Add git and base configuration

---
+++ conf.d/base.fish
@@ -0,0 +1,8 @@
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
+export EDITOR=nvim
+export PATH="/opt/homebrew/opt/ruby/bin:$PATH"
+export PATH="/opt/homebrew/Cellar/python@3.12/3.12.2_1/libexec/bin:/opt/homebrew/bin:$PATH"
+export PATH="$HOME/.local/bin:$PATH"
+export PATH="$HOME/.cargo/bin:$PATH"
+export PATH="$HOME/go/bin:$PATH"
---
+++ conf.d/git.fish
@@ -0,0 +1,8 @@
+alias gl="git pull --rebase"
+alias gp="git push"
+alias gst="git status"
+alias grv="git remote --verbose"
+alias gaa="git add --all"
+alias ga="git add"
+alias gc="git commit"
+alias glo="git log --oneline"
