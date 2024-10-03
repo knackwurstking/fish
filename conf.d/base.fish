23d320652eeb4a60b0d32161e9277fc1b01ae7da
Udo Bauer <knackwurstking.tux@gmail.com>
2024-10-03 11:01:15 +0200

    Add android sdk export

--- conf.d/base.fish
+++ conf.d/base.fish
@@ -6,3 +6,4 @@ export PATH="/opt/homebrew/Cellar/python@3.12/3.12.2_1/libexec/bin:/opt/homebrew
 export PATH="$HOME/.local/bin:$PATH"
 export PATH="$HOME/.cargo/bin:$PATH"
 export PATH="$HOME/go/bin:$PATH"
+export ANDROID_NDK_HOME=$HOME/Library/Android/sdk/ndk/26.2.11394342
